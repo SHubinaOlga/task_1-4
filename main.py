4e8306fb6edcd43c90cb6db261b59f956a25c1ea
SHubinaOlga <olga.shubina85@yandex.ru>
2024-08-05 21:36:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+print("1st program")
+print((9**0.5)*5)
+
+print("2st program")
+print(9.99>9.98)
+print(1000!=1000.1)
+
+print("3st program")
+rast1=2*2+2
+print(rast1)
+
+rast2=2*(2+2)
+print(rast2)
+
+print(rast1==rast2)
+
+print("4st program")
+name = '123.456'
+print(name[-3])
+
+print(float(name)*10)
+
+print(int(1234.56))
+print(1234/10)
+
